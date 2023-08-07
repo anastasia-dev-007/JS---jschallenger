45dbf56af832e9e90e611926e4436085c7c63d8e
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-08-07 16:05:30 +0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -89,3 +89,64 @@ function equalElements( arr ) {
 // console.log(equalElements([1, 1, 1, 2])); // Output: false (Not all elements are equal)
 // console.log(equalElements(['10', 10, 10, 10])); // Output: false (Not all elements are equal)
 // console.log(equalElements([])); // Output: false (Empty array cannot have all elements equal)
+
+
+function mergeArr(...arrays) {
+    return arrays.flat()
+}
+
+//OR
+
+function mergeArr2(...arrays) {
+    return [].concat(...arrays);
+}
+
+// console.log(mergeArr([1, 2, 3], [4, 5, 6])); // Output: [1, 2, 3, 4, 5, 6]
+// console.log(mergeArr(['a', 'b', 'c'], [4, 5, 6])); // Output: ['a', 'b', 'c', 4, 5, 6]
+// console.log(mergeArr2([true, true], [1, 2], ['a', 'b'])); // Output: [true, true, 1, 2, 'a', 'b']
+
+// Sort array by object property
+// Write a function that takes an array of objects as argument. Sort the array by property b in ascending order. Return the sorted array
+
+const sortByObject = (arr) => {
+   return arr.sort((a,b) => a.b - b.b);
+}
+// console.log(sortByObject([{ a: 1, b: 2 }, { a: 5, b: 4 }]));
+// // Output: [{ a: 1, b: 2 }, { a: 5, b: 4 }]
+
+// console.log(sortByObject([{ a: 2, b: 10 }, { a: 5, b: 4 }]));
+// // Output: [{ a: 5, b: 4 }, { a: 2, b: 10 }]
+
+// console.log(sortByObject([{ a: 1, b: 7 }, { a: 2, b: 1 }]));
+// Output: [{ a: 2, b: 1 }, { a: 1, b: 7 }]
+
+
+// Merge two arrays with duplicate values
+// Write a function that takes two arrays as arguments. Merge both arrays and remove duplicate values. Sort the merge result in ascending order. Return the resulting array
+
+
+function mergeUniqueArr (a, b) {
+    return [...new Set([...a, ...b])].sort((x, y) => x - y);
+}
+console.log(mergeUniqueArr([1, 2, 3], [3, 4, 5])); // Expected [ 1, 2, 3, 4, 5 ]
+console.log(mergeUniqueArr([-10, 22, 333, 42], [-11, 5, 22, 41, 42])); //Expected [ -11, -10, 5, 22, 41,  42, 333]  }
+
+
+// Sum up all array elements with values greater than
+// Write a function that takes an array (a) and a number (b) as arguments. Sum up all array elements with a value greater than b. Return the sum
+
+
+// Create a range of numbers
+// Write a function that takes two numbers (min and max) as arguments. Return an array of numbers in the range min to max
+
+
+// Group array of strings by first letter
+// Write a function that takes an array of strings as argument. Group those strings by their first letter. Return an object that contains properties with keys representing first letters. The values should be arrays of strings containing only the corresponding strings. For example, the array ['Alf', 'Alice', 'Ben'] should be transformed to. { a: ['Alf', 'Alice'], b: ['Ben']}
+
+
+// Define an array with conditional elements
+// Write a function that takes an array with arbitrary elements and a number as arguments. Return a new array, the first element should be either the given number itself. or zero if the number is smaller than 6. The other elements should be the elements of the original array. Try not to mutate the original array
+
+
+// Get every nth element of array
+// Write a function that takes an array (a) and a value (n) as arguments. Save every nth element in a new array. Return the new array
